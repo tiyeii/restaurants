4c9e6f3ab359abf76903e5cbfaccddbc04b84139
tiyeii <dariotj@gmail.com>
2022-09-15 15:23:55 -0700

    add Navegacion

---
+++ navigations/Navigation.js
@@ -0,0 +1,58 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer, TabActions } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Restaurants from '../screens/Restaurants'
+import Favorites from '../screens/Favorites'
+import TopRestaurants from '../screens/TopRestaurants'
+import Search from '../screens/Search'
+import Account from '../screens/Account'
+
+const Tab = createBottomTabNavigator()
+
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen 
+                name="restaurants"
+                component={Restaurants}
+                options={{ title: "Restaurantes" }} 
+            
+            />
+
+        <Tab.Screen 
+                name="favorites"
+                component={Favorites}
+                options={{ title: "Favoritos" }} 
+            
+            />
+        
+        <Tab.Screen 
+                name="top-restaurants"
+                component={TopRestaurants}
+                options={{ title: "Top 5" }} 
+            
+            />
+
+         <Tab.Screen 
+                name="search"
+                component={Search}
+                options={{ title: "Buscar" }} 
+            
+            />
+        
+        <Tab.Screen 
+                name="account"
+                component={Account}
+                options={{ title: "Perfil" }} 
+            
+            />
+
+
+        </Tab.Navigator>
+    </NavigationContainer>
+  
+  )
+}
